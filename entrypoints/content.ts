f66d82b0c5a169e19a8eddae59bb566611c89155
burak unver <burakhanunver@gmail.com>
2025-09-01 13:02:04 +0300

    add editable logic

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,31 @@
 export default defineContentScript({
   matches: ["<all_urls>"],
   main() {
-    console.log("Hello content.")
+    browser.runtime.onMessage.addListener(message => {
+      if (message.action === "edit-text") {
+        console.log("Edit text", message.editStatus)
+        if (message.editStatus) {
+          enableEditMode()
+        } else {
+          disableEditMode()
+        }
+      }
+    })
   },
 })
+
+function enableEditMode() {
+  let elements = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6")
+  elements.forEach(el => {
+    el.setAttribute("contenteditable", "true")
+    el.style.outline = "1px dashed red"
+  })
+}
+
+function disableEditMode() {
+  let elements = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6")
+  elements.forEach(el => {
+    el.removeAttribute("contenteditable")
+    el.style.outline = ""
+  })
+}
