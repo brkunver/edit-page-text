b0208ef818af9f02ba1d518eb48b18a323f11c47
burak unver <burakhanunver@gmail.com>
2025-09-01 13:02:12 +0300

    add toggle logic

--- entrypoints/popup/main.ts
+++ entrypoints/popup/main.ts
@@ -1,10 +1,13 @@
 import "./style.css"
 
+let editStatus: boolean = false
 const button = document.getElementById("edit-text") as HTMLButtonElement
 button.addEventListener("click", () => {
+  editStatus = !editStatus
+  button.textContent = editStatus ? "Deactivate edit mode" : "Activate edit mode"
   browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
     if (tabs[0].id) {
-      browser.tabs.sendMessage(tabs[0].id, { action: "edit-text" })
+      browser.tabs.sendMessage(tabs[0].id, { action: "edit-text", editStatus })
     } else {
       console.log("No active tab found")
     }
