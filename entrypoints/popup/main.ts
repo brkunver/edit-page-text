0094717ebf4cdafda104220afebfab6c52eff3bd
burak unver <burakhanunver@gmail.com>
2025-09-01 13:07:57 +0300

    change logic to two buttons

--- entrypoints/popup/main.ts
+++ entrypoints/popup/main.ts
@@ -1,13 +1,21 @@
 import "./style.css"
 
-let editStatus: boolean = false
-const button = document.getElementById("edit-text") as HTMLButtonElement
-button.addEventListener("click", () => {
-  editStatus = !editStatus
-  button.textContent = editStatus ? "Deactivate edit mode" : "Activate edit mode"
+const activateButton = document.getElementById("activate-button") as HTMLButtonElement
+activateButton.addEventListener("click", () => {
   browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
     if (tabs[0].id) {
-      browser.tabs.sendMessage(tabs[0].id, { action: "edit-text", editStatus })
+      browser.tabs.sendMessage(tabs[0].id, { action: "edit-text", editStatus: true })
+    } else {
+      console.log("No active tab found")
+    }
+  })
+})
+
+const deactivateButton = document.getElementById("deactivate-button") as HTMLButtonElement
+deactivateButton.addEventListener("click", () => {
+  browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
+    if (tabs[0].id) {
+      browser.tabs.sendMessage(tabs[0].id, { action: "edit-text", editStatus: false })
     } else {
       console.log("No active tab found")
     }
